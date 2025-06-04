54514eee8f9b74b5802cb8b4ce0fd61ca5a7c40c
Noelisoa <alainrakotoarivony926@gmail.com>
2025-06-05 00:05:19 +0300

    Display all students

--- app.js
+++ app.js
@@ -1,37 +1,34 @@
 const app = angular.module("myApp", []);
 
-app.controller("StudentController", function($scope) {
-    let students = [];
+app.controller("StudentController", function ($scope) {
 
     $scope.classes = ["6ème", "5ème", "4ème", "3ème", "2nde", "1ère", "Terminale"];
-    
+
     $scope.student = {
-        nom : "",
-        prenom : "",
-        dateNaissance : "",
-        classe : ""
+        nom: "",
+        prenom: "",
+        dateNaissance: "",
+        classe: ""
     }
 
-    $scope.ajouterEtudiant = function() {
+    $scope.ajouterEleve = function () {
         if ($scope.student.nom && $scope.student.prenom && $scope.student.dateNaissance && $scope.student.classe) {
-            students.push({
+            $scope.listeEleve.push({
                 nom: $scope.student.nom,
                 prenom: $scope.student.prenom,
-                dateNaissance: $scope.student.dateNaissance,
+                dateNaissance: $scope.student.dateNaissance.toISOString().split('T')[0].split('-').reverse().join('-'), 
                 classe: $scope.student.classe
             });
             $scope.student = {
-                nom : "",
-                prenom : "",
-                dateNaissance : "",
-                classe : ""
+                nom: "",
+                prenom: "",
+                dateNaissance: "",
+                classe: ""
             };
         } else {
             alert("Veuillez remplir tous les champs.");
         }
     }
 
-    $scope.listeEtudiants = function() {
-        return students;
-    }
+    $scope.listeEleve = []
 });
